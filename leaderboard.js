2d9fc461e71eaac56491171553febddb90fd3ecc
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-03-14 20:04:05 -0400

    Adding reader

--- leaderboard.js
+++ leaderboard.js
@@ -13,6 +13,26 @@ if (Meteor.isClient) {
 		return Teams.find({ _id: { $in: game.teams } }); //{}, {sort: {name: 1}});
 	};
 
+	Template.reader.questions = function() {
+		var game = Games.findOne();
+		if (!game)
+			return;
+		var round = Rounds.findOne(game.round_id);
+		var packet = Packets.findOne(round.packet_id);
+		console.log(packet);
+		return packet.tossups;
+	};
+	Template.reader.question = function() {
+		if (!(Template.reader.questions()))
+			return;
+		if (!Session.get('cur_question'))
+			Session.set('cur_question', 0);
+		return Template.reader.questions()[Session.get('cur_question')];
+	}
+	Template.reader.question_index = function() {
+		return Session.get('cur_question') + 1;
+	}
+
 	Template.team.score = function() {
 		var players = Players.find({ 'team_id': this._id });
 		var players_array = players.fetch(); // because Cursor doesn't have reduce, only fetch
@@ -40,6 +60,19 @@ if (Meteor.isClient) {
 
 			// Assuming that Session.get("selected_player") === this._id because the buttons only show up when player is selected
 			Players.update(this._id, { $inc: { 'score': points } });
+		}
+	});
+
+	Template.reader.events({
+		'click input': function (event) {
+			var trg = event.srcElement || event.target;
+			var inc = +trg.getAttribute('data-inc');
+
+			var cur_question = Session.get('cur_question');
+			var num_questions = Template.reader.questions().length;
+			Session.set('cur_question',
+				Math.min(num_questions - 1, Math.max(0, cur_question + inc))
+			);
 		}
 	});
 
